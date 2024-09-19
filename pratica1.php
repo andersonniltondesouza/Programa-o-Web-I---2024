7e810c167f846539daa68881b99d75981bee59cd
Anderson Nilton de Souza <andersonniltondesouza@gmail.com>
2024-09-19 19:24:53 -0300

    commit

---
+++ pratica1.php
@@ -0,0 +1,9 @@
+<?php
+
+define(constant_name:"nome",value:"Anderson Nilton");
+define(constant_name:"sobrenome",value:"de Souza")
+$nomecompleto = nome. "". sobrenome;
+
+echo($nomecompleto);
+
+?>
