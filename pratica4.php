3d8714a62e881c46826f56f54424d9f4f8bf325e
Anderson Nilton de Souza <andersonniltondesouza@gmail.com>
2024-09-19 21:03:55 -0300

    commit

--- pratica4.php
+++ pratica4.php
@@ -3,15 +3,19 @@
 $salario1= 1000;
 $salario2= 2000;
 
-$salario2=$salario1;
-
 ++$salario2;
 
-$salario1*=1.1;
+for ($x = 0; $x <= 100; $x++) {
+    $salario1=$salario1+1;
+    echo "The number is: $salario1 <br>";
+    
+    if ($x === 49) {
+        break;
+    }   
+}
 
-
-for ($x = 0; $x <= 10; $x++) {
-    echo "The number is: $x <br>";
-    }
+if ($salario1 < $salario2) {
+    echo ("$salario1");
+}
 
 ?>
