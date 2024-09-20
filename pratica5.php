cc17be7da44dd5846a00ef163e6383de21915eaa
Anderson Nilton de Souza <andersonniltondesouza@gmail.com>
2024-09-19 21:20:14 -0300

    commit

---
+++ pratica5.php
@@ -0,0 +1,10 @@
+<?php
+
+$disciplina = array("Engenharia de Software II","Banco de Dados II","Administração e Sistemas de Informação","Programação Web I","Estrutura de Dados II");
+$professor = array("Julian","Marco","Sandro","Cléber","Fernando");
+
+for ($x = 0; $x < 5; $x++) {
+    echo("Disciplina - $disciplina[$x], Professor - $professor[$x] <br>");
+}
+
+?>
