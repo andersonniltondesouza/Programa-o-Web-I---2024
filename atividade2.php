509a1529192e33e6a3b9f6c31ff68ce6acc0cc68
Anderson Nilton de Souza <andersonniltondesouza@gmail.com>
2024-10-01 21:35:47 -0300

    commit

---
+++ atividade2.php
@@ -0,0 +1,16 @@
+<!-- 2. Crie um programa para testar se um número é divisível por 2.
+Caso for verdade escrever a frase “Valor divisível por 2”
+Caso for falso escrever a frase “O valor não é divisível por 2” -->
+
+<?php
+
+$valor = 2;
+
+if ($valor % 2 ==0) {
+    echo("Valor é divisível por 2"); 
+}
+else {
+    echo("Valor não é divisível por 2");
+}
+
+?>
