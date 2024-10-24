1560eb8328f47fc0f155f7ab73e847a99b86662f
Anderson Nilton de Souza <andersonniltondesouza@gmail.com>
2024-10-24 19:45:04 -0300

    commit

--- model/index.php
+++ model/index.php
@@ -1,19 +1,19 @@
 <?php
-    require_once "model\pessoa.php";
-    require_once "model\contato.php";
-    require_once "model\\endereco.php";    
+    require_once "pessoa.php";
+    require_once "contato.php";
+    require_once "endereco.php";    
 
     /*Instanciar um objeto da classe pessoa*/
     $objetoPessoa = new \app\model\Pessoa();
     $objetoPessoa->setNome("Anderson");
     $objetoPessoa->setSobreNome("Nilton de Souza");
-    $objetoPessoa->setDataNascimento(new DateTime("****-**-**"));
+    $objetoPessoa->setDataNascimento(new DateTime("2005-03-24"));
     $objetoPessoa->setTipo(1);
     $objetoPessoa->setCpfCnpj("000.000.000-00");
 
     $objetoPessoa->getTelefone()->setTipo(1);
     $objetoPessoa->getTelefone()->setNome("Telefone Pessoal");
-    $objetoPessoa->getTelefone()->setValor("** ****-****");
+    $objetoPessoa->getTelefone()->setValor("00 00000-0000");
     echo "Nome Completo: " . $objetoPessoa->getNomeCompleto() . "<br>"; 
     echo "Idade: " . $objetoPessoa->getIdade();
 ?>
